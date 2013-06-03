b74ad97552eded2fc731aa671d35e0d76f95e521
Adam Wolfe Gordon <awg@xvx.ca>
2013-06-03 12:18:26 -0600

    Change font size to 8, which works when DPI is set correctly

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "InconsolataLGC:size=10";
+static char font[] = "InconsolataLGC:size=8";
 static int borderpx = 2;
 static char shell[] = "/bin/zsh";
 
